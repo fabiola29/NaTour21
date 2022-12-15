72dfe907194234f51bd3140b065d4ff505397961
Fabiola Salomone <60442414+fabiola29@users.noreply.github.com>
2022-12-15 13:19:12 +0100

    Add files via upload
    Codice applicazione

---
+++ Codice/NaTour22/app/app/src/androidTest/java/com/example/natour2022/ExampleInstrumentedTest.java
@@ -0,0 +1,23 @@
+package com.example.natour2022;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+
+    //Controllo che il nome del package corrisponda alla stringa "com.natour2022"
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.NaTour2022", appContext.getPackageName());
+    }
+}
